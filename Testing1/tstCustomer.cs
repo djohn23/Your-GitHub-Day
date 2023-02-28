ef656953e4314ceed84837301747cb6cafc894ad
Dawn Johnston <djohns02@dmu.ac.uk>
2023-02-28 18:00:55 +0000

    Added New tstCustomer - no red lines appearing

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //Test to see it exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
